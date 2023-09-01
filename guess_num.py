7a4f3f4744ffcad87cf2dd69c62dc552aee787bc
Lewis Yang <xoxo50214l@hotmail.com>
2023-09-01 23:37:10 +0800

    Final Version

--- guess_num.py
+++ guess_num.py
@@ -1,10 +1,14 @@
 import random
 
-r = random.randint(1,100)
+start = input('請決定數字範圍開始值：')
+start = int(start)
+end = input('請決定數字範圍結束值：')
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1
-	num = input('請從1~100隨機挑選一個數字！')
+	num = input('請從您設定的範圍內隨機挑選一個數字！')
 	num = int(num)
 	if num == r:
 		print('恭喜您！您猜中了！')
